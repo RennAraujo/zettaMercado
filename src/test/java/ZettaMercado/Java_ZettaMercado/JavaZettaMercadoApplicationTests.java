49290381efd83b70c55b1bda9ecba353ffa66e28
Rennan Araujo <rennan771@gmail.com>
2025-07-05 21:27:40 -0300

    Primeiro commit

---
+++ src/test/java/ZettaMercado/Java_ZettaMercado/JavaZettaMercadoApplicationTests.java
@@ -0,0 +1,13 @@
+package ZettaMercado.Java_ZettaMercado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaZettaMercadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
